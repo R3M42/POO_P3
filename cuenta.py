4bde00eef3a52fe47688f3e94a3e3ace00ba8bf4
R3M42 <rolandoe3m@gmail.com>
2023-10-31 17:19:40 -0400

    Version 31-10-2023_03

--- cuenta.py
+++ cuenta.py
@@ -1,3 +1,7 @@
 class Cuenta:
-    def __init__(self):
-        print("Construyendo el objeto...{}".format(self))
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construyendo el objeto...{}".format(slef))
+        self.numero = 123
+        self.titular = "Anacleto Metralla"
+        self.saldo = 100.0
+        self.limite = 1000.0
